ea8ddb3597b5ab1769487a042218fc8975bed957
masarakki <masaki@hisme.net>
2012-08-25 15:10:42 +0900

    formatting

--- src/net/hisme/masaki/Access2ch.java
+++ src/net/hisme/masaki/Access2ch.java
@@ -10,45 +10,45 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class Access2ch {
-	public static ArrayList<String[]> threads(String host, String board) {
-		ArrayList<String[]> res = new ArrayList<String[]>();
-		try {
-			URL uri = new URL("http://" + host + "/" + board + "/subject.txt");
-			HttpURLConnection http = (HttpURLConnection) uri.openConnection();
-			http.setRequestMethod("GET");
-			http.addRequestProperty("Accept-Encoding", "gzip");
-			http.addRequestProperty("User-Agent", "Monazilla/1.00");
-			http.addRequestProperty("Connection", "close");
-			http.connect();
+  public static ArrayList<String[]> threads(String host, String board) {
+    ArrayList<String[]> res = new ArrayList<String[]>();
+    try {
+      URL uri = new URL("http://" + host + "/" + board + "/subject.txt");
+      HttpURLConnection http = (HttpURLConnection) uri.openConnection();
+      http.setRequestMethod("GET");
+      http.addRequestProperty("Accept-Encoding", "gzip");
+      http.addRequestProperty("User-Agent", "Monazilla/1.00");
+      http.addRequestProperty("Connection", "close");
+      http.connect();
 
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					new GZIPInputStream(http.getInputStream()), "SHIFT_JIS"));
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				res.add(line.split(".dat<>"));
-			}
-			Collections.sort(res, new Comparator<String[]>() {
-				public int compare(String[] a, String[] b) {
-					double c = Double.parseDouble(b[0]) - Double.parseDouble(a[0]);
-					if (c > 0)
-						return 1;
-					if (c == 0)
-						return 0;
-					return -1;
-				}
-			});
+      BufferedReader reader = new BufferedReader(new InputStreamReader(
+          new GZIPInputStream(http.getInputStream()), "SHIFT_JIS"));
+      String line = null;
+      while ((line = reader.readLine()) != null) {
+        res.add(line.split(".dat<>"));
+      }
+      Collections.sort(res, new Comparator<String[]>() {
+        public int compare(String[] a, String[] b) {
+          double c = Double.parseDouble(b[0]) - Double.parseDouble(a[0]);
+          if (c > 0)
+            return 1;
+          if (c == 0)
+            return 0;
+          return -1;
+        }
+      });
 
-			reader.close();
-			http.disconnect();
-		} catch (java.net.MalformedURLException e) {
-			// return e.toString();
-		} catch (java.io.IOException e) {
-			// return e.toString();
-		} catch (java.lang.IllegalStateException e) {
-			// return e.toString();
-		} catch (NullPointerException e) {
-			// return e.toString();
-		}
-		return res;
-	}
+      reader.close();
+      http.disconnect();
+    } catch (java.net.MalformedURLException e) {
+      // return e.toString();
+    } catch (java.io.IOException e) {
+      // return e.toString();
+    } catch (java.lang.IllegalStateException e) {
+      // return e.toString();
+    } catch (NullPointerException e) {
+      // return e.toString();
+    }
+    return res;
+  }
 }
--- src/net/hisme/masaki/img_collector/ImageCollector.java
+++ src/net/hisme/masaki/img_collector/ImageCollector.java
@@ -21,109 +21,108 @@ import android.util.Log;
 import net.hisme.masaki.Access2ch;
 
 public class ImageCollector extends Activity {
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		Button button1 = (Button) findViewById(R.id.Button01);
-		Button button2 = (Button) findViewById(R.id.Button02);
 
-		button1.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View view) {
-				openBoard("kilauea.bbspink.com", "megami");
-			}
-		});
-		button2.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View view) {
-				openBoard("pele.bbspink.com", "neet4pink");
-			}
-		});
-	}
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    Button button1 = (Button) findViewById(R.id.Button01);
+    Button button2 = (Button) findViewById(R.id.Button02);
 
-	private void openBoard(final String host, final String board) {
-		TextView text = (TextView) findViewById(R.id.TextView01);
-		text.setText("Open Board...");
-		final Handler handler = new Handler();
-		new Thread() {
-			public void run() {
-				final ArrayList<String[]> threads = Access2ch.threads(host, board);
-				handler.post(new Runnable() {
-					public void run() {
-						ImageCollector.this.displayThreadList(host, board, threads);
-					}
-				});
-			}
-		}.start();
-	}
+    button1.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View view) {
+        openBoard("kilauea.bbspink.com", "megami");
+      }
+    });
+    button2.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View view) {
+        openBoard("pele.bbspink.com", "neet4pink");
+      }
+    });
+  }
 
-	private void displayThreadList(final String host, final String board,
-			final ArrayList<String[]> threads) {
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-				ImageCollector.this, R.layout.thread);
-		for (String[] thread : threads) {
-			adapter.add(thread[1]);
-		}
-		ListView list = (ListView) findViewById(R.id.ListView01);
-		list.setAdapter(adapter);
-		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view, int position,
-					long id) {
-				postAddRequest(host, board, threads.get(position)[0]);
-			}
-		});
-	}
+  private void openBoard(final String host, final String board) {
+    TextView text = (TextView) findViewById(R.id.TextView01);
+    text.setText("Open Board...");
+    final Handler handler = new Handler();
+    new Thread() {
+      public void run() {
+        final ArrayList<String[]> threads = Access2ch.threads(host, board);
+        handler.post(new Runnable() {
+          public void run() {
+            ImageCollector.this.displayThreadList(host, board, threads);
+          }
+        });
+      }
+    }.start();
+  }
 
-	private void postAddRequest(final String host, final String board,
-			final String thread) {
-		TextView text = (TextView) findViewById(R.id.TextView01);
-		text.setText("send request...");
-		final Handler handler = new Handler();
-		new Thread() {
-			public void run() {
-				final String thread_uri = "http://" + host + "/test/read.cgi/" + board
-						+ "/" + thread + "/";
-				try {
-					URL uri = new URL(
-							"http://hisme.net/~masaki/img_collector/_add_thread.php");
-					HttpURLConnection http = (HttpURLConnection) uri.openConnection();
-					http.setRequestMethod("POST");
-					http.setDoOutput(true);
-					http.setDoInput(true);
-					PrintWriter writer = new PrintWriter(http.getOutputStream());
+  private void displayThreadList(final String host, final String board,
+      final ArrayList<String[]> threads) {
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+        ImageCollector.this, R.layout.thread);
+    for (String[] thread : threads) {
+      adapter.add(thread[1]);
+    }
+    ListView list = (ListView) findViewById(R.id.ListView01);
+    list.setAdapter(adapter);
+    list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      public void onItemClick(AdapterView<?> parent, View view, int position,
+          long id) {
+        postAddRequest(host, board, threads.get(position)[0]);
+      }
+    });
+  }
 
-					writer.print("uri=" + thread_uri);
-					writer.flush();
-					writer.close();
+  private void postAddRequest(final String host, final String board,
+      final String thread) {
+    TextView text = (TextView) findViewById(R.id.TextView01);
+    text.setText("send request...");
+    final Handler handler = new Handler();
+    new Thread() {
+      public void run() {
+        final String thread_uri = "http://" + host + "/test/read.cgi/" + board
+            + "/" + thread + "/";
+        try {
+          URL uri = new URL("http://hisme.net/~masaki/img_collector/_add_thread.php");
+          HttpURLConnection http = (HttpURLConnection) uri.openConnection();
+          http.setRequestMethod("POST");
+          http.setDoOutput(true);
+          http.setDoInput(true);
+          PrintWriter writer = new PrintWriter(http.getOutputStream());
 
-					BufferedReader reader = new BufferedReader(new InputStreamReader(http
-							.getInputStream()));
-					final String post_result = reader.readLine();
-					handler.post(new Runnable() {
-						public void run() {
-							TextView text = (TextView) findViewById(R.id.TextView01);
-							text.setText(post_result + " : " + thread_uri);
-						}
-					});
-					reader.close();
+          writer.print("uri=" + thread_uri);
+          writer.flush();
+          writer.close();
 
-					http.disconnect();
-				} catch (java.net.MalformedURLException e) {
-					ImageCollector.log(e.toString());
-				} catch (java.io.IOException e) {
-					ImageCollector.log(e.toString());
-				} catch (java.lang.IllegalStateException e) {
-					ImageCollector.log(e.toString());
-				} catch (NullPointerException e) {
-					ImageCollector.log(e.toString());
-				} catch (Exception e) {
-					ImageCollector.log(e.toString());
-				}
-			}
-		}.start();
-	}
+          BufferedReader reader = new BufferedReader(new InputStreamReader(http
+              .getInputStream()));
+          final String post_result = reader.readLine();
+          handler.post(new Runnable() {
+            public void run() {
+              TextView text = (TextView) findViewById(R.id.TextView01);
+              text.setText(post_result + " : " + thread_uri);
+            }
+          });
+          reader.close();
 
-	static void log(String str) {
-		Log.d("[ImageCollector]", str);
-	}
+          http.disconnect();
+        } catch (java.net.MalformedURLException e) {
+          ImageCollector.log(e.toString());
+        } catch (java.io.IOException e) {
+          ImageCollector.log(e.toString());
+        } catch (java.lang.IllegalStateException e) {
+          ImageCollector.log(e.toString());
+        } catch (NullPointerException e) {
+          ImageCollector.log(e.toString());
+        } catch (Exception e) {
+          ImageCollector.log(e.toString());
+        }
+      }
+    }.start();
+  }
+
+  static void log(String str) {
+    Log.d("[ImageCollector]", str);
+  }
 }
