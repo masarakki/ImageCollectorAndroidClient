04e3ad18613438ed8ae2c1c3d7209a5027a4e2c2
masarakki <masaki@hisme.net>
2012-08-27 06:10:34 +0900

    fix for mona

--- src/net/hisme/masaki/img_collector/ImageCollector.java
+++ src/net/hisme/masaki/img_collector/ImageCollector.java
@@ -4,12 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Button;
-import android.widget.TextView;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
+import android.widget.Toast;
 
 import java.io.PrintWriter;
 import java.io.InputStreamReader;
@@ -17,66 +18,82 @@ import java.io.BufferedReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import net.hisme.masaki.access_2ch.*;
+import net.hisme.masaki.mona.*;
 
 public class ImageCollector extends Activity {
+
+  private final Board[] boards = {
+      new Board("女神", "kilauea.bbspink.com", "megami"),
+      new Board("ニー速pink", "pele.bbspink.com", "neet4pink")
+  };
+
+  private Toast toast;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    Button button1 = (Button) findViewById(R.id.Button01);
-    Button button2 = (Button) findViewById(R.id.Button02);
-
-    button1.setOnClickListener(new View.OnClickListener() {
-      public void onClick(View view) {
-        openBoard("kilauea.bbspink.com", "megami");
-      }
-    });
-    button2.setOnClickListener(new View.OnClickListener() {
-      public void onClick(View view) {
-        openBoard("pele.bbspink.com", "neet4pink");
-      }
-    });
+    LinearLayout buttons = (LinearLayout) findViewById(R.id.buttons);
+    for (final Board board : boards) {
+      Button button = new Button(ImageCollector.this);
+      button.setText(board.getBoardName());
+      button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          updateThreadList(board);
+        }
+      });
+      buttons.addView(button);
+    }
   }
 
-  private void openBoard(final String host, final String board) {
-    TextView text = (TextView) findViewById(R.id.TextView01);
-    text.setText("Open Board...");
+  private void updateThreadList(final Board board) {
     final Handler handler = new Handler();
+    showMessage("Open board...");
+
     new Thread() {
       public void run() {
-        final ThreadList threads = Access2ch.threads(host, board);
+        final ThreadList thread_list = board.getThreadList();
+        hideMessage();
         handler.post(new Runnable() {
           public void run() {
-            ImageCollector.this.displayThreadList(host, board, threads);
+            ImageCollector.this.displayThreadList(board, thread_list);
           }
         });
       }
     }.start();
   }
 
-  private void displayThreadList(final String host, final String board, final ThreadList threads) {
+  private void displayThreadList(final Board board, final ThreadList threads) {
     ArrayAdapter<String> adapter = new ArrayAdapter<String>(ImageCollector.this, R.layout.thread);
-    for (BbsThread thread : threads) {
+    for (Thread2ch thread : threads) {
       adapter.add(thread.getTitle());
     }
-    ListView list = (ListView) findViewById(R.id.ListView01);
+    ListView list = (ListView) findViewById(R.id.thread_list);
     list.setAdapter(adapter);
     list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
       public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        postAddRequest(host, board, threads.get(position).getThreadId());
+        postAddRequest(MonaUtils.getThreadUrl(board, threads.get(position)));
       }
     });
   }
 
-  private void postAddRequest(final String host, final String board, final String thread_id) {
-    TextView text = (TextView) findViewById(R.id.TextView01);
-    text.setText("send request...");
+  private void hideMessage() {
+    if (toast != null)
+      toast.cancel();
+  }
+
+  private void showMessage(String message) {
+    hideMessage();
+    toast = Toast.makeText(ImageCollector.this, message, Toast.LENGTH_SHORT);
+    toast.show();
+  }
+
+  private void postAddRequest(final String thread_url) {
+    showMessage("Send request...");
     final Handler handler = new Handler();
     new Thread() {
       public void run() {
-        final String thread_uri = String.format("http://%s/test/read.cgi/%s/%s/", host, board, thread_id);
         try {
           URL uri = new URL("http://hisme.net/~masaki/img_collector/_add_thread.php");
           HttpURLConnection http = (HttpURLConnection) uri.openConnection();
@@ -85,7 +102,7 @@ public class ImageCollector extends Activity {
           http.setDoInput(true);
           PrintWriter writer = new PrintWriter(http.getOutputStream());
 
-          writer.print("uri=" + thread_uri);
+          writer.print("uri=" + thread_url);
           writer.flush();
           writer.close();
 
@@ -93,8 +110,7 @@ public class ImageCollector extends Activity {
           final String post_result = reader.readLine();
           handler.post(new Runnable() {
             public void run() {
-              TextView text = (TextView) findViewById(R.id.TextView01);
-              text.setText(post_result + " : " + thread_uri);
+              showMessage(post_result);
             }
           });
           reader.close();
